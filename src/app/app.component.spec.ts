db849ba6f3a222b802df7dfcb9173841a5e65a47
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 19:27:16 +0300

    added test suite: async

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -949,3 +949,36 @@ describe('Manually ticking the Jasmine Clock', function() {
     });
   });
 });
+
+describe('Asynchronous specs', function() {
+  var value;
+
+  beforeEach(function(done) {
+    setTimeout(function() {
+      value = 0;
+      done();
+    }, 1);
+  });
+
+  it('should support async execution of test preparation and expectations', function(done) {
+    value++;
+    expect(value).toBeGreaterThan(0);
+    done();
+  });
+
+  describe('long asynchronous specs', function() {
+    beforeEach(function(done) {
+      done();
+    }, 1000);
+
+    it('takes a long time', function(done) {
+      setTimeout(function() {
+        done();
+      }, 9000);
+    }, 10000);
+
+    afterEach(function(done) {
+      done();
+    }, 1000);
+  });
+});
