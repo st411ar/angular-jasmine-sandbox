b28b7a18734b34d9d29fad037d9b82adceeb15aa
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 18:45:21 +0300

    added test suite: jasmine clock date mocking

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -937,4 +937,15 @@ describe('Manually ticking the Jasmine Clock', function() {
     jasmine.clock().tick(50);    
     expect(timerCallback.calls.count()).toEqual(2);
   });
+
+  describe('Mocking the Date object', function() {
+    it('mock the Date object and sets it to a given time', function() {
+      let baseTime = new Date(2013, 9, 23);
+
+      jasmine.clock().mockDate(baseTime);
+
+      jasmine.clock().tick(50);
+      expect(new Date().getTime()).toEqual(baseTime.getTime() + 50);
+    });
+  });
 });
