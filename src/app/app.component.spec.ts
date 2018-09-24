b4b48400ddb04452f555d736730c895842c7e6a1
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 15:08:36 +0300

    added test suite: spy example with "and.throwError"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -528,3 +528,23 @@ describe('A spy, when configured with an alternate implementation', function() {
     expect(fetchedBar).toEqual(1001);
   });
 });
+
+describe('A spy, when configured to throw an error', function() {
+  var foo, bar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      }
+    };
+
+    spyOn(foo, 'setBar').and.throwError('quux');
+  });
+
+  it('throws the value', function() {
+    expect(function() {
+      foo.setBar(123);
+    }).toThrowError('quux');
+  });
+});
