826360d14d11d62f87b6bf873d93957304f5c1ca
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 17:04:09 +0300

    added test suite: jasmine.anything matcher

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -759,3 +759,20 @@ describe('jasmine.any', function() {
     });
   });
 });
+
+describe('jasmine.anything', function() {
+  it('matches anything', function() {
+    expect(1).toEqual(jasmine.anything());
+  });
+
+  describe('when used with a spy', function() {
+    it('is useful when the argument can be ignored', function() {
+      var foo = jasmine.createSpy('foo');
+      foo(12, function() {
+        return false;
+      });
+
+      expect(foo).toHaveBeenCalledWith(12, jasmine.anything());
+    });
+  });
+});
