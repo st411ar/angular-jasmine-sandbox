6f42d6c0e186953400497462ceb6539d5e041be2
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 15:04:51 +0300

    added test suite: spy example with "and.callFake"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -494,3 +494,37 @@ describe('A spy, when configured to fake a series of return values', function() 
     expect(foo.getBar()).toBeUndefined();
   });
 });
+
+describe('A spy, when configured with an alternate implementation', function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      },
+      getBar: function() {
+        return bar;
+      }
+    };
+
+    spyOn(foo, 'getBar').and.callFake(function(args, can, be, received) {
+      return 1001;
+    });
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it('tracks that the spy was calld', function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it('should not affect other functions', function() {
+    expect(bar).toEqual(123);
+  });
+
+  it('when called returns the requested value', function() {
+    expect(fetchedBar).toEqual(1001);
+  });
+});
