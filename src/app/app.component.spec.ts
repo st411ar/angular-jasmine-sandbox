87781ada329c8f88bbeba5b2d769f164480df0f8
Vitaly Kiselev <st411ar@ya.ru>
2018-09-24 14:36:24 +0300

    added test suite: spy example with "and.callThrough"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -396,3 +396,35 @@ describe('A spy', function() {
     expect(bar).toBeNull();
   });
 });
+
+describe('A spy, when configured to call through', function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
+      },
+      getBar: function() {
+        return bar;
+      }
+    };
+
+    spyOn(foo, 'getBar').and.callThrough();
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it('tracks that the spy was called', function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it('should not affect other functions', function() {
+    expect(bar).toEqual(123);
+  });
+
+  it('when called returns the requested value', function() {
+    expect(fetchedBar).toEqual(123);
+  });
+});
